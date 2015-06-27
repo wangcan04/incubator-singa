0a8cf1b50974e65aea865addfb940b390e2670fd
zhaojing <zhaojing@comp.nus.edu.sg>
2015-06-27 15:52:18 +0800

    visulization tool

--- src/proto/plot.py
+++ src/proto/plot.py
@@ -139,14 +139,18 @@ def tile_raster_images(X, img_shape, tile_shape, tile_spacing=(0, 0),
         return out_array
 
 import model_pb2 as model
+import sys
 bp=model.BlobProto()
-fd=open("../../examples/mnistDBM/visualization/15000", "rb")
+fd=open(sys.argv[1], "rb")
+#fd=open("../../examples/mnistDBM/visualization/5000", "rb")
 bp.ParseFromString(fd.read())
 import numpy as np
 mat=np.array(bp.data).reshape((bp.height, bp.width))
+print bp.height, bp.width
 
 image = Image.fromarray(tile_raster_images(
         X = mat.T,
         img_shape=(28, 28), tile_shape=(10, 10),
         tile_spacing=(1, 1)))
-image.save('filters_RBM15000.png')
+image.save(os.path.splitext(sys.argv[1])[0], '.jpg')
+#image.save('filters_RBM5000-1.png')
